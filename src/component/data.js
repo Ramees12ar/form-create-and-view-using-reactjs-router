2f57762d4d58c4d18b23b1d09495c9569ea5d514
Ramees12ar <ramees.ar04@gmail.com>
2021-03-18 01:09:10 +0530

    data and details js updated

--- src/component/data.js
+++ src/component/data.js
@@ -4,19 +4,31 @@ class Data extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            datas: [],
-            fname: "",
-            lname: "",
-            age: "",
-            dob: "",
-            place: ""
+            data: [],
+            sort: [],
         }
     }
-    componentDidMount() {
-        var arr =JSON.parse(localStorage.getItem("localData"));
-        this.setState({data: arr});
-        console.log(this.state.datas);
-        console.log(arr);
+    async componentDidMount() {
+        var str =await (localStorage.getItem("localData"));
+        if(str !=null){
+            var arr= JSON.parse(str);
+            await this.setState({data: arr});
+        }
+        else{
+            alert("No data available. Please add data")
+        }
+        //console.log(this.state.data);
+    }
+
+    //array filtered based on view click
+    detail = (k) => {
+        var arr = this.state.data;
+        let newData = arr.filter(function (id,index) {
+            return index === k;
+        });
+        this.setState({ sort: newData }); 
+        //console.log(this.state.sort);
+        localStorage.setItem("sortData", JSON.stringify(newData));       
     }
     render() {
         return (
@@ -28,16 +40,17 @@ class Data extends Component {
                             <th>Last Name</th>
                             <th>View</th>
                         </tr>
-                        <tr>
-                            <td>ramees</td>
-                            <td>ar</td>
-                            <button>view</button>
-                        </tr>
-                        <tr>
-                            <td>ramees</td>
-                            <td>ar</td>
-                            <td><button>view</button></td>
-                        </tr>
+                        {
+                        this.state.data.map((i, k) => {
+                            return (
+                                <tr id={k}>
+                                        <td class="cap">{i.fname}</td>
+                                        <td class="upp">{i.lname}</td>
+                                        <td><button onClick={() => {this.detail(k)}}>view</button></td>
+                                </tr>
+                            )
+                        })
+                        }
                     </tbody>
                 </table>
             </div>
--- src/component/detail.js
+++ src/component/detail.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 import './detail.css';
 class View extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            sort: [],
+        }
+    }
+    async componentDidMount() {
+        var arr =await JSON.parse(localStorage.getItem("sortData"));
+        await this.setState({sort: arr});
+        //console.log(this.state.sort);
+    }
     render() {
         return (
             <div className="head3">
@@ -13,13 +24,19 @@ class View extends Component {
                         <th>Date of Birth</th>
                         <th>Place</th>
                     </tr>
-                    <tr>
-                        <td>Ramees</td>
-                        <td>ar</td>
-                        <td>23</td>
-                        <td>25/04/1982</td>
-                        <td>ekm</td>
-                    </tr>
+                    {
+                    this.state.sort.map((i, k) => {
+                        return (
+                            <tr id={k}>
+                                <td class="cap">{i.fname}</td>
+                                <td class="upp">{i.lname}</td>
+                                <td>{i.age}</td>
+                                <td>{i.dob}</td>
+                                <td>{i.place}</td>
+                            </tr>
+                            )
+                        })
+                    }
                 </tbody>
             </table>
             </div>
